441d7015b9ca5563307ff6eed30ee14926afbf0a
Lydia <lydiazly2@gmail.com>
2025-03-15 17:37:26 -0700

    update setup params

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # setup.py
 from setuptools import setup, find_packages
 
 setup(
+    author="lydiazly",
     name="importlens",
+    description="Get all import statements in the caller's frame. Originally created for LeetCode's Python3 environment.",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    license="MIT",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    url="https://github.com/lydiazly/python-importlens",
+    version="0.1.0",
 )
